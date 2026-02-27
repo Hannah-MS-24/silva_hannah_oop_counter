91f4d9bf7c5113cdbd52ac6dd1d6f36244c53eb7
Hannah-MS-24 <hannahmelo24@gmail.com>
2026-02-27 15:10:32 -0500

    add
    add folders

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import { Counter } from './counter.js';
+
+const counter1 = new Counter("#counter-container1", 0);
+const counter2 = new Counter("#counter-container2", 5);
+const newCounter = new Counter("#counterCon", 10);
+const counter4 = new Counter("#counterCon2", 0);
+
+//const step10 = new StepCounter(id, starting number, step);
